4c685e3620d528ee51b813e53976739e6b338f37
Anatol Hancharou <a.potteroff@gmail.com>
2018-02-21 23:46:09 +0300

    quadratic-equation solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  equation = equation.split(' ').join('');
+  var a = equation.split('*x^2')[0];
+  var b = equation.split('*x^2')[1].split('*x')[0];
+  var c = equation.split('*x^2')[1].split('*x')[1];
+  var d = b**2 - 4 * a * c;
+  var x = [];
+  x[0] = Math.round((-b + Math.sqrt(d)) / (2 * a));
+  x[1] = Math.round((-b - Math.sqrt(d)) / (2 * a));
+  return x.sort((a, b) => a - b);
 }
